onomyprotocol/cosmos-sdk

Vesting account withdrawal reward limitation.
For the vesting account, with the coins still locked in vesting, the reward amount will be equally spread in the vesting
based on the vesting account type strategy: for all types, the OriginalVesting amount will be increased by the reward amount. For the PeriodicVestingAccount
the amount will be additionally spread among the periods based on the amount in each period.

x/distribution/keeper/delegation.go
@@ -5,6 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	vestexported "github.com/cosmos/cosmos-sdk/x/auth/vesting/exported"
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -171,8 +172,8 @@ func (k Keeper) withdrawDelegationRewards(ctx sdk.Context, val stakingtypes.Vali
 		if err != nil {
 			return nil, err
 		}
+		k.addVestingRewards(ctx, del.GetDelegatorAddr(), coins)
 	}
-
 	// update the outstanding rewards and the community pool only if the
 	// transaction was successful
 	k.SetValidatorOutstandingRewards(ctx, del.GetValidatorAddr(), types.ValidatorOutstandingRewards{Rewards: outstanding.Sub(rewards)})
@@ -189,4 +190,13 @@ func (k Keeper) withdrawDelegationRewards(ctx sdk.Context, val stakingtypes.Vali
 	k.DeleteDelegatorStartingInfo(ctx, del.GetValidatorAddr(), del.GetDelegatorAddr())
 
 	return coins, nil
+}
+
+func (k Keeper) addVestingRewards(ctx sdk.Context, delAddr sdk.AccAddress, amt sdk.Coins) {
+	vacc, ok := k.authKeeper.GetAccount(ctx, delAddr).(vestexported.VestingAccount)
+	if !ok || vacc.GetVestingCoins(ctx.BlockTime()).IsZero(){
+		return
+	}
+	vacc.AddOriginalVesting(amt)
+	k.authKeeper.SetAccount(ctx, vacc)
 }

AaronKutch/onomy-sdk

x/distribution/keeper/delegation.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	vestexported "github.com/cosmos/cosmos-sdk/x/auth/vesting/exported"
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -171,8 +173,8 @@ func (k Keeper) withdrawDelegationRewards(ctx sdk.Context, val stakingtypes.Vali
 		if err != nil {
 			return nil, err
 		}
+		k.addVestingRewards(ctx, del.GetDelegatorAddr(), finalRewards)
 	}
-
 	// update the outstanding rewards and the community pool only if the
 	// transaction was successful
 	k.SetValidatorOutstandingRewards(ctx, del.GetValidatorAddr(), types.ValidatorOutstandingRewards{Rewards: outstanding.Sub(rewards)})
@@ -209,4 +211,13 @@ func (k Keeper) withdrawDelegationRewards(ctx sdk.Context, val stakingtypes.Vali
 	)
 
 	return finalRewards, nil
+}
+
+func (k Keeper) addVestingRewards(ctx sdk.Context, delAddr sdk.AccAddress, amt sdk.Coins) {
+	vacc, ok := k.authKeeper.GetAccount(ctx, delAddr).(vestexported.VestingAccount)
+	if !ok || vacc.GetVestingCoins(ctx.BlockTime()).IsZero() {
+		return
+	}
+	vacc.AddOriginalVesting(amt)
+	k.authKeeper.SetAccount(ctx, vacc)
 }